e4529914e2fe0354bb414ddb7f9b40c542b069af
Elledgef <102770608+Elledgef@users.noreply.github.com>
2022-04-06 16:05:55 -0700

    Update Project2a.py

--- Project2a.py
+++ Project2a.py
@@ -4,7 +4,5 @@ num2 = float(input())
 num3 = float(input())
 num4 = float(input())
 num5 = float(input())
-print("The sum is:", num1+num2+num3+num4+num5)
 print("The Average:", num1+num2+num3+num4+num5/5)
-
 
