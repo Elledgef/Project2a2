a68649f989f75a923dac1999141e56ea0923386c
faith elledge <faithelledge@10-249-86-46.wireless.oregonstate.edu>
2022-04-06 15:50:06 -0700

    Initial commit

---
+++ Project2a.py
@@ -0,0 +1,10 @@
+print("Please enter five numbers")
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+num5 = float(input())
+print("The sum is:", num1+num2+num3+num4+num5)
+print("The Average:", num1+num2+num3+num4+num5/5)
+
+
